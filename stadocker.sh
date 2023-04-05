5a01fc7a8ea3c6f5c2ff5551515e2647b89ab8f3
Matt Venn <matt@mattvenn.net>
2023-04-05 17:19:08 +0200

    path for sta log

--- stadocker.sh
+++ stadocker.sh
@@ -6,13 +6,4 @@ docker run --rm \
     -v $PDK_ROOT:$PDK_ROOT \
     --user 1000:1000 \
     efabless/openlane:a35b64aa200c91e9eb7dde56db787d6b4c0ea12a-amd64 \
-    /bin/bash -c "cd $(pwd) ; sh verilog/gl/sta.sh | tee sta.log"
-
-    #sh verilog/gl/sta.sh
-    #-v /home/matt/work/asic-workshop/shuttle7/openlane/designs:/openlane/install \
-    #-v /home/matt/work/asic-workshop/shuttle7/pdk:/home/matt/work/asic-workshop/shuttle7/pdk \
-    #-v /tmp/.X11-unix:/tmp/.X11-unix \
-    #-v /home/matt/.Xauthority:/.Xauthority \
-
-    #-e PDK_ROOT=/home/matt/work/asic-workshop/shuttle7/pdk \
-    #-e OPENLANE_ROOT=/home/matt/work/asic-workshop/shuttle9/openlane \
+    /bin/bash -c "cd $(pwd) ; sh verilog/gl/sta.sh | tee $(pwd)/sta.log"
