af40f79a3b36e5f154f068e9031f493076c15bed
Matt Venn <matt@mattvenn.net>
2023-04-25 10:39:51 +0200

    numbered designs render

--- post_build.sh
+++ post_build.sh
@@ -2,7 +2,9 @@
 set -e
 make compress
 
-./tt/configure.py --update-image --dump-markdown datasheet.md --dump-pdf datasheet.pdf
+./tt/configure.py --update-image
+./tt/tt_annotate.py
+./tt/configure.py --dump-markdown datasheet.md --dump-pdf datasheet.pdf
 ./tt/configure.py --build-hugo-content ~/work/asic-workshop/tinytapeout/tinytapeout_www/content/runs/tt03/
 ./tt/configure.py --metrics
 
